649ddd9a3f091951cdd44b49547ddbb59e5c2aea
lukun <lukun@CentOS65-242.(none)>
2016-07-04 11:39:19 +0800

    update

---
+++ abs/bashpid.sh
@@ -0,0 +1,12 @@
+#!/bin/bash4
+
+echo "\$\$ outside of subshell = $$"                              # 9602
+echo "\$BASH_SUBSHELL  outside of subshell = $BASH_SUBSHELL"      # 0
+echo "\$BASHPID outside of subshell = $BASHPID"                   # 9602
+
+echo
+
+( echo "\$\$ inside of subshell = $$"                             # 9602
+  echo "\$BASH_SUBSHELL inside of subshell = $BASH_SUBSHELL"      # 1
+  echo "\$BASHPID inside of subshell = $BASHPID" )                # 9603
+  # Note that $$ returns PID of parent process.
