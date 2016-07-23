a01d810aab911fbeb8aa74286400cfefc184a206
Kun Lu <yimng.lk@gmail.com>
2016-07-23 17:55:36 +0800

    update

--- unix/ocamlunix/basic.ml
+++ unix/ocamlunix/basic.ml
@@ -63,3 +63,68 @@ let pp_expression =
          in if pr2 > pr then res else parenthesis res
      | _ -> ppl pr exp
    in ppl 0
+
+let pp_command = function 
+     Rem s     ->  "REM " ^ s
+   | Goto n    ->  "GOTO " ^ (string_of_int n)
+   | Print e   ->  "PRINT " ^ (pp_expression e)
+   | Input v   ->  "INPUT " ^ v
+   | If (e,n)  ->  "IF "^(pp_expression e)^" THEN "^(string_of_int n)
+   | Let (v,e) ->  "LET " ^ v ^ " = " ^ (pp_expression e)
+
+let pp_line line = (string_of_int line.num) ^ "  " ^ (pp_command line.cmd)
+
+type lexeme = Lint of int 
+             | Lident of string 
+             | Lsymbol of string  
+             | Lstring of string
+             | Lend
+
+type string_lexer = {string:string; mutable current:int; size:int }
+
+let init_lex s = { string=s; current=0 ; size=String.length s }
+let forward cl = cl.current <- cl.current+1
+let forward_n cl n = cl.current <- cl.current+n
+
+let extract pred cl = 
+   let st = cl.string and pos = cl.current in
+   let rec ext n = if n<cl.size && (pred st.[n]) then ext (n+1) else n in 
+   let res = ext pos
+   in cl.current <- res ; String.sub cl.string pos (res-pos)
+
+let extract_int = 
+    let is_int = function '0'..'9' -> true | _ -> false  
+    in function cl -> int_of_string (extract is_int cl)
+ let extract_ident =
+    let is_alpha_num = function 
+      'a'..'z' | 'A'..'Z' | '0' .. '9' | '_' -> true 
+    | _ -> false  
+    in extract is_alpha_num
+
+exception LexerError
+
+let  rec lexer cl = 
+   let lexer_char c = match c with 
+       ' ' 
+     | '\t'     -> forward cl ; lexer cl 
+     | 'a'..'z' 
+     | 'A'..'Z' -> Lident (extract_ident cl)
+     | '0'..'9' -> Lint (extract_int cl)
+     | '"'      -> forward cl ; 
+                   let res = Lstring (extract ((<>) '"') cl) 
+                   in forward cl ; res 
+     | '+' | '-' | '*' | '/' | '%' | '&' | '|' | '!' | '=' | '(' | ')'  -> 
+                   forward cl; Lsymbol (String.make 1 c)
+     | '<' 
+     | '>'      -> forward cl; 
+                   if cl.current >= cl.size then Lsymbol (String.make 1 c)
+                   else  let cs = cl.string.[cl.current] 
+                         in ( match (c,cs) with 
+                                ('<','=') -> forward cl; Lsymbol "<=" 
+                              | ('>','=') -> forward cl; Lsymbol ">="
+                              | ('<','>') -> forward cl; Lsymbol "<>"
+                              |     _     -> Lsymbol (String.make 1 c) )
+     | _ -> raise LexerError
+   in 
+      if cl.current >= cl.size then Lend 
+      else lexer_char cl.string.[cl.current]
