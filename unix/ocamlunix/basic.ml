05af98fd57bc6a51d8639308fc11381b5d7f0590
Kun Lu <yimng.lk@gmail.com>
2016-08-12 17:51:52 +0800

    update

--- unix/ocamlunix/basic.ml
+++ unix/ocamlunix/basic.ml
@@ -128,3 +128,53 @@ let  rec lexer cl =
    in 
       if cl.current >= cl.size then Lend 
       else lexer_char cl.string.[cl.current]
+
+type exp_elem = 
+     Texp of expression   (* expression       *)
+   | Tbin of bin_op       (* binary operator  *)
+   | Tunr of unr_op       (* unary operator   *)
+   | Tlp                  (* left parenthesis *)
+
+exception ParseError
+
+let unr_symb = function 
+   "!" -> NOT | "-" -> UMINUS | _ -> raise ParseError 
+let bin_symb = function
+     "+" -> PLUS | "-" -> MINUS | "*" -> MULT | "/" -> DIV | "%" -> MOD 
+   | "=" -> EQUAL | "<" -> LESS | "<=" -> LESSEQ | ">" -> GREAT 
+   | ">=" -> GREATEQ | "<>" -> DIFF | "&" -> AND | "|" -> OR 
+   | _ -> raise ParseError 
+let tsymb s = try Tbin (bin_symb s) with ParseError -> Tunr (unr_symb s)
+
+let reduce pr = function 
+     (Texp e)::(Tunr op)::st  when  (priority_uop op) >= pr
+        -> (Texp (ExpUnr (op,e)))::st
+   | (Texp e1)::(Tbin op)::(Texp e2)::st  when  (priority_binop op) >= pr
+        -> (Texp (ExpBin (e2,op,e1)))::st 
+   | _ -> raise ParseError
+
+
+let rec stack_or_reduce lex stack = match lex , stack with 
+     Lint n ,  _      ->  (Texp (ExpInt n))::stack 
+   | Lident v ,  _    ->  (Texp (ExpVar v))::stack 
+   | Lstring s , _    ->  (Texp (ExpStr s))::stack
+   | Lsymbol "(" , _  ->  Tlp::stack
+   | Lsymbol ")" , (Texp e)::Tlp::st  ->  (Texp e)::st
+   | Lsymbol ")" , _ -> stack_or_reduce lex (reduce 0 stack) 
+   | Lsymbol s , _ 
+        -> let symbol = 
+             if s<>"-" then tsymb s 
+             (* remove the ambiguity of the ``-'' symbol           *)
+             (* according to the last exp element put on the stack *)
+             else match stack 
+                  with (Texp _)::_  ->  Tbin MINUS 
+                                | _ ->  Tunr UMINUS 
+           in ( match symbol with 
+                  Tunr op  ->  (Tunr op)::stack 
+                | Tbin op  -> 
+                    ( try stack_or_reduce lex (reduce (priority_binop op) 
+                                               stack )
+                      with ParseError -> (Tbin op)::stack )
+                | _ -> raise ParseError )
+   | _ , _ -> raise ParseError
+
