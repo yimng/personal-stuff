dcd81ee1febc55343db4ba13e5cd96a9aeca7732
Kun Lu <yimng.lk@gmail.com>
2016-08-15 17:49:02 +0800

    update

---
+++ abs/commands/readpipe.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# readpipe.sh
+# This example contributed by Bjon Eriksson.
+
+### shopt -s lastpipe
+
+last="(null)"
+cat $0 |
+while read line
+do
+    echo "{$line}"
+    last=$line
+done
+
+echo
+echo "++++++++++++++++++++++"
+printf "\nAll done, last: $last\n" #  The output of this line
+                                   #+ changes if you uncomment line 5.
+                                   #  (Bash, version -ge 4.2 required.)
+
+exit 0  # End of code.
+        # (Partial) output of script follows.
+        # The 'echo' supplies extra brackets.
+
+#############################################
+
