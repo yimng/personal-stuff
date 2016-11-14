b40496279b125b56bc85a677492ed499c2cc6015
Kun Lu <yimng.lk@gmail.com>
2016-11-14 23:38:29 +0800

    update

---
+++ abs/advance/psub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# psub.bash
+
+# As inspired by Diego Molina (thanks!).
+
+declare -a array0
+while read
+do
+  array0[${#array0[@]}]="$REPLY"
+done < <( sed -e 's/bash/CRASH-BANG!/' $0 | grep bin | awk '{print $1}' )
+#  Sets the default 'read' variable, $REPLY, by process substitution,
+#+ then copies it into an array.
+
+echo "${array0[@]}"
+
+exit $?
---
+++ abs/advance/subshell-test.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+# subshell-test.sh
+
+(
+# Inside parentheses, and therefore a subshell . . .
+while [ 1 ]   # Endless loop.
+do
+  echo "Subshell running . . ."
+done
+)
+
+#  Script will run forever,
+#+ or at least until terminated by a Ctl-C.
+
+exit $?  # End of script (but will never get here).
