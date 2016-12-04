005e9ed7d34a4731e067788bec51b810b5e3331a
Kun Lu <yimng.lk@gmail.com>
2016-12-04 23:42:10 +0800

    update

---
+++ abs/2/self-delete.sh
@@ -0,0 +1,12 @@
+#!/bin/rm
+# Self-deleting script.
+
+# Nothing much seems to happen when you run this... except that the file disappears.
+
+WHATEVER=85
+
+echo "This line will never print (betcha!)."
+
+exit $WHATEVER  # Doesn't matter. The script will not exit here.
+                # Try an echo $? after script termination.
+                # You'll get a 0, not a 85.
