9b145971761d36bde1e7c29097addcb66fa17ab9
Kun Lu <yimng.lk@gmail.com>
2017-02-16 14:18:49 +0800

    update

---
+++ abs/17/erase.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# erase.sh: Using "stty" to set an erase character when reading input.
+
+oldsttysetting=$(stty -g)
+echo -n "What is your name? "
+read name                      #  Try to backspace
+                               #+ to erase characters of input.
+                               #  Problems?
+echo "Your name is $name."
+
+stty erase '#'                 #  Set "hashmark" (#) as erase character.
+echo -n "What is your name? "
+read name                      #  Use # to erase last character typed.
+echo "Your name is $name."
+
+stty ${oldsttysetting}
+exit 0
+
+# Even after the script exits, the new key value remains set.
+# Exercise: How would you reset the erase character to the default value?
