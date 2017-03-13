4cc5fc93a53d7957ac3652a6c31d9be741a51d77
Kun Lu <yimng.lk@gmail.com>
2017-03-13 04:35:29 -0400

    update

--- abs/20/avoid-subshell.sh
+++ abs/20/avoid-subshell.sh
@@ -21,16 +21,16 @@ echo "Number of lines read = $Lines"     # 0
 echo "------------------------"
 
 
-exec 3<> myfile.txt
-while read line <&3
+#exec 3<> myfile.txt
+while read line# <&3
 do {
   echo "$line"
   (( Lines++ ));                   #  Incremented values of this variable
                                    #+ accessible outside loop.
                                    #  No subshell, no problem.
 }
-done
-exec 3>&-
+done < myfile.txt
+#exec 3>&-
 
 echo "Number of lines read = $Lines"     # 8
 
---
+++ abs/20/redihere.sh
@@ -0,0 +1,23 @@
+# This example by Albert Siersema
+# Used with permission (thanks!).
+
+function doesOutput()
+ # Could be an external command too, of course.
+ # Here we show you can use a function as well.
+{
+  ls -al *.sh | awk '{print $5,$9}'
+}
+
+
+nr=0          #  We want the while loop to be able to manipulate these and
+totalSize=0   #+ to be able to see the changes after the 'while' finished.
+
+while read fileSize fileName ; do
+  echo "$fileName is $fileSize bytes"
+  let nr++
+  totalSize=$((totalSize+fileSize))   # Or: "let totalSize+=fileSize"
+done<<EOF
+$(doesOutput)
+EOF
+
+echo "$nr files totaling $totalSize bytes"
