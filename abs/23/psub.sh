32d45bda39c040d1de0cbace99ea2b9eaedf21a7
Kun Lu <yimng.lk@gmail.com>
2017-03-13 06:25:48 -0400

    update

--- abs/23/psub.sh
+++ abs/23/psub.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # psub.bash
-set -x
 # As inspired by Diego Molina (thanks!).
 
 declare -a array0
 while read
 do
+  echo "$REPLY"
+  echo ${#array0[@]}
   array0[${#array0[@]}]="$REPLY"
 done < <( sed -e 's/bash/CRASH-BANG!/' $0 | grep bin | awk '{print $1}' )
 #  Sets the default 'read' variable, $REPLY, by process substitution,
