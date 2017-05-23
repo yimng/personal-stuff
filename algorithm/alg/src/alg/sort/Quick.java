e503ae096f17a7634b36adf2a16c97464305fe23
Lukun <yimng.lk@gmail.com>
2017-05-23 11:47:24 +0800

    update

---
+++ algorithm/alg/src/alg/sort/Quick.java
@@ -0,0 +1,48 @@
+package alg.sort;
+
+import stdio.StdRandom;
+
+public class Quick extends Sort {
+
+	private Quick() {
+	}
+
+	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+		assert isSorted(a);
+	}
+
+	public static void sort(Comparable[] a, int lo, int hi) {
+		if (lo >= hi)
+			return;
+		int j = partition(a, lo, hi);
+		sort(a, lo, j - 1);
+		sort(a, j + 1, hi);
+	}
+
+	private static int partition(Comparable[] a, int lo, int hi) {
+		int i = lo;
+		int j = hi + 1;
+		Comparable v = a[lo];
+		while (true) {
+			while (less(a[++i], v)) {
+				if (i == hi) {
+					break;
+				}
+
+			}
+			while (less(v, a[--j])) {
+				if (j == lo) {
+					break;
+				}
+			}
+			if (i >= j) {
+				break;
+			}
+			exch(a, i, j);
+		}
+		exch(a, lo, j);
+		return j;
+	}
+}
