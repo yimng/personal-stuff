e67e6849257ed230136052ddbfe8e09fbcb27f9f
Lukun <yimng.lk@gmail.com>
2017-06-04 18:07:11 +0800

    update

---
+++ algorithm/alg/src/alg/find/BST.java
@@ -0,0 +1,202 @@
+package alg.find;
+
+public class BST<Key extends Comparable<Key>, Value> {
+	private Node root;
+
+	private class Node {
+		private Key key;
+		private Value val;
+		private Node left, right;
+		private int N;
+
+		public Node(Key key, Value val, int N) {
+			this.key = key;
+			this.val = val;
+			this.N = N;
+		}
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	public int size(Node x) {
+		if (x == null)
+			return 0;
+		else
+			return x.N;
+	}
+
+	public Value get(Key key) {
+		return get(root, key);
+	}
+
+	private Value get(Node node, Key key) {
+		if (node == null)
+			return null;
+
+		int c = key.compareTo(node.key);
+		if (c < 0) {
+			return get(node.left, key);
+		} else if (c > 0) {
+			return get(node.right, key);
+		} else {
+			return node.val;
+		}
+	}
+
+	public void put(Key key, Value val) {
+		root = put(root, key, val);
+	}
+
+	private Node put(Node x, Key key, Value value) {
+		if (x == null)
+			return new Node(key, value, 1);
+		int cmp = key.compareTo(key);
+		if (cmp < 0)
+			x.left = put(x.left, key, value);
+		else if (cmp > 0)
+			x.right = put(x.right, key, value);
+		else
+			x.val = value;
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public Key max() {
+		return max(root).key;
+	}
+
+	private Node max(Node x) {
+		if (x.right == null)
+			return x;
+		return max(x.right);
+	}
+
+	public Key min() {
+		return min(root).key;
+	}
+
+	private Node min(Node x) {
+		if (x.left == null)
+			return x;
+		return min(x.left);
+	}
+
+	public Key floor(Key key) {
+		Node x = floor(root, key);
+		if (x == null)
+			return null;
+		return x.key;
+	}
+
+	private Node floor(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0)
+			return x;
+		if (cmp < 0)
+			return floor(x.left, key);
+		Node t = floor(x.right, key);
+		if (t != null)
+			return t;
+		else
+			return x;
+	}
+
+	public Key ceiling(Key key) {
+		Node x = ceiling(root, key);
+		if (x == null)
+			return null;
+		return x.key;
+	}
+
+	private Node ceiling(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp == 0)
+			return x;
+		if (cmp > 0)
+			return ceiling(x.right, key);
+		Node t = ceiling(x.left, key);
+		if (t != null)
+			return t;
+		else
+			return x;
+	}
+
+	public Node select(int k) {
+		return null;
+	}
+
+	public int rank(Key key) {
+		return rank(key, root);
+	}
+
+	private int rank(Key key, Node x) {
+		if (x == null)
+			return 0;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0)
+			return rank(key, x.left);
+		else if (cmp > 0)
+			return 1 + size(x.left) + rank(key, x.right);
+		else
+			return size(x.left);
+	}
+
+	public void delete(Key key) {
+		root = delete(root, key);
+	}
+
+	private Node delete(Node x, Key key) {
+		if (x == null)
+			return null;
+		int cmp = key.compareTo(x.key);
+		if (cmp < 0) {
+			x.left = delete(x.left, key);
+		} else if (cmp > 0) {
+			x.right = delete(x.right, key);
+		} else {
+			if (x.right == null) {
+				return x.left;
+			}
+			if (x.left == null) {
+				return x.right;
+			}
+			Node t = x;
+			x = min(t.right);
+			x.right = deleteMin(t.right);
+			x.left = t.left;
+		}
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public void deleteMin() {
+		root = deleteMin(root);
+	}
+
+	private Node deleteMin(Node x) {
+		if (x.left == null)
+			return x.right;
+		x.left = deleteMin(x.left);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+
+	public void deleteMax() {
+		root = deleteMax(root);
+	}
+	
+	private Node deleteMax(Node x) {
+		if (x.right == null) {
+			return x.left;
+		}
+		x.right = deleteMax(x.right);
+		x.N = size(x.left) + size(x.right) + 1;
+		return x;
+	}
+}
