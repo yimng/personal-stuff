2725e9b741f6f711633ffbb42c5fdf7e8aa600a3
Lukun <yimng.lk@gmail.com>
2017-06-19 16:37:13 +0800

    update

--- algorithm/alg/src/alg/search/LinearProbingHashST.java
+++ algorithm/alg/src/alg/search/LinearProbingHashST.java
@@ -1,6 +1,6 @@
 package alg.search;
 
-import alg.basic.ResizingArrayQueue;
+import alg.basic.LinkedQueue;
 import stdio.StdIn;
 import stdio.StdOut;
 
@@ -17,10 +17,10 @@ public class LinearProbingHashST<Key, Value> {
 	}
 
 	public LinearProbingHashST(int capacity) {
-		m = capacity;
+		keys = (Key[]) new Object[capacity];
+		vals = (Value[]) new Object[capacity];
 		n = 0;
-		keys = (Key[]) new Object[m];
-		vals = (Value[]) new Object[m];
+		m = capacity;
 	}
 
 	public int size() {
@@ -32,36 +32,37 @@ public class LinearProbingHashST<Key, Value> {
 	}
 
 	public boolean contains(Key key) {
-		if (key == null)
-			throw new IllegalArgumentException("argument to contains is null");
 		return get(key) != null;
 	}
 
 	// hash function for keys - returns value between 0 and M-1
 	private int hash(Key key) {
-		return (key.hashCode() & 0x7fffffff) % m;
+		return (key.hashCode() & 0x7ffffff) % m;
 	}
 
 	private void resize(int capacity) {
-
+		LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
+		for (int i = 0; i < m; i++) {
+			if (keys[i] != null) {
+				temp.put(keys[i], vals[i]);
+			}
+		}
+		keys = temp.keys;
+		vals = temp.vals;
+		m = temp.m;
 	}
 
 	public void put(Key key, Value val) {
-		if (key == null)
-			throw new IllegalArgumentException("first argument to put() is null");
-
 		if (val == null) {
 			delete(key);
 			return;
 		}
-
-		// double table size if 50% full
-		if (n >= m / 2)
+		if (2 * n >= m) {
 			resize(2 * m);
-
+		}
 		int i;
 		for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
-			if (keys[i].equals(key)) {
+			if (keys[i] == key) {
 				vals[i] = val;
 				return;
 			}
@@ -74,32 +75,26 @@ public class LinearProbingHashST<Key, Value> {
 	public Value get(Key key) {
 		if (key == null)
 			throw new IllegalArgumentException("argument to get() is null");
-		for (int i = hash(key); keys[i] != null; i = (i + 1) % m)
-			if (keys[i].equals(key))
+		for (int i = hash(key); keys[i] != null; i = (i + 1) % m) {
+			if (keys[i] == key) {
 				return vals[i];
+			}
+		}
 		return null;
 	}
 
 	public void delete(Key key) {
-		if (key == null)
-			throw new IllegalArgumentException("argument to delete() is null");
-		if (!contains(key))
+		if (!contains(key)) {
 			return;
-
-		// find position i of key
+		}
 		int i = hash(key);
-		while (!key.equals(keys[i])) {
+		while (keys[i] != key) {
 			i = (i + 1) % m;
 		}
-
-		// delete key and associated value
 		keys[i] = null;
 		vals[i] = null;
-
-		// rehash all keys in same cluster
 		i = (i + 1) % m;
 		while (keys[i] != null) {
-			// delete keys[i] an vals[i] and reinsert
 			Key keyToRehash = keys[i];
 			Value valToRehash = vals[i];
 			keys[i] = null;
@@ -108,21 +103,21 @@ public class LinearProbingHashST<Key, Value> {
 			put(keyToRehash, valToRehash);
 			i = (i + 1) % m;
 		}
-
 		n--;
 
-		// halves size of array if it's 12.5% full or less
-		if (n > 0 && n <= m / 8)
+		if (n > 0 && n <= m / 8) {
 			resize(m / 2);
-
+		}
 		assert check();
 	}
 
 	public Iterable<Key> keys() {
-		ResizingArrayQueue<Key> queue = new ResizingArrayQueue<Key>();
-		for (int i = 0; i < m; i++)
-			if (keys[i] != null)
+		LinkedQueue<Key> queue = new LinkedQueue<Key>();
+		for (int i = 0; i < m; i++) {
+			if (keys[i] != null) {
 				queue.enqueue(keys[i]);
+			}
+		}
 		return queue;
 	}
 
--- algorithm/alg/src/alg/search/SeparateChainingHashST.java
+++ algorithm/alg/src/alg/search/SeparateChainingHashST.java
@@ -15,27 +15,26 @@ public class SeparateChainingHashST<Key, Value> {
 	}
 
 	public SeparateChainingHashST(int M) {
-		this.M = M;
-		st = (SequentialSearchST<Key, Value>[]) new SequentialSearchST[M];
+		st = new SequentialSearchST[M];
 		for (int i = 0; i < M; i++) {
 			st[i] = new SequentialSearchST<Key, Value>();
 		}
+		this.M = M;
 	}
 
 	private void resize(int chains) {
-		SeparateChainingHashST<Key, Value> temp = new SeparateChainingHashST(chains);
+		SeparateChainingHashST temp = new SeparateChainingHashST(chains);
 		for (int i = 0; i < M; i++) {
 			for (Key key : st[i].keys()) {
 				temp.put(key, st[i].get(key));
 			}
 		}
-		this.M = temp.M;
-		this.N = temp.N;
 		this.st = temp.st;
+		this.M = temp.M;
 	}
 
 	private int hash(Key key) {
-		return (key.hashCode() & 0x7fffffff) % M;
+		return (key.hashCode() & 0x7ffffff) % M;
 	}
 
 	public int size() {
@@ -47,57 +46,41 @@ public class SeparateChainingHashST<Key, Value> {
 	}
 
 	public boolean contains(Key key) {
-		if (key == null)
-			throw new IllegalArgumentException("argument to contains() is null");
 		return get(key) != null;
 	}
 
 	public Value get(Key key) {
-		if (key == null)
-			throw new IllegalArgumentException("argument to get() is null");
 		int i = hash(key);
 		return st[i].get(key);
 	}
 
 	public void put(Key key, Value val) {
-		if (key == null)
-			throw new IllegalArgumentException("first argument to put() is null");
 		if (val == null) {
 			delete(key);
 			return;
 		}
-
-		// double table size if average length of list >= 10
-		if (N >= 10 * M)
-			resize(2 * M);
-
 		int i = hash(key);
-		if (!st[i].contains(key))
+		if (!st[i].contains(key)) {
 			N++;
+		}
 		st[i].put(key, val);
 	}
 
 	public void delete(Key key) {
-		if (key == null)
-			throw new IllegalArgumentException("argument to delete() is null");
-
 		int i = hash(key);
-		if (st[i].contains(key))
+		if (st[i].contains(key)) {
 			N--;
+		}
 		st[i].delete(key);
-
-		// halve table size if average length of list <= 2
-		if (M > INIT_CAPACITY && N <= 2 * M)
-			resize(M / 2);
 	}
 
 	public Iterable<Key> keys() {
 		LinkedQueue<Key> queue = new LinkedQueue<Key>();
-		for (int i = 0; i < M; i++) {
-			for (Key key : st[i].keys())
-				queue.enqueue(key);
-		}
-		return queue;
+        for (int i = 0; i < M; i++) {
+            for (Key key : st[i].keys())
+                queue.enqueue(key);
+        }
+        return queue;
 	}
 	
 	public static void main(String[] args) { 
