11ac6e93ebad6880f8d016f96b2137cf3ca7ce41
Lukun <yimng.lk@gmail.com>
2017-06-21 18:11:55 +0800

    update

---
+++ algorithm/alg/src/alg/graph/DepthFirstPaths.java
@@ -0,0 +1,44 @@
+package alg.graph;
+
+import alg.basic.LinkedStack;
+
+public class DepthFirstPaths {
+
+	private boolean[] marked;
+	private int[] edgeTo;
+	private final int s;
+
+	public DepthFirstPaths(Graph G, int s) {
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		this.s = s;
+		dfs(G, s);
+	}
+
+	private void dfs(Graph G, int v) {
+		marked[v] = true;
+		for (int w : G.adj(v)) {
+			if (!marked[w]) {
+				edgeTo[w] = v;
+				dfs(G, w);
+			}
+		}
+	}
+
+	public boolean hasPathTo(int v) {
+		return marked[v];
+	}
+
+	public Iterable<Integer> pathTo(int v) {
+		if (!hasPathTo(v)) {
+			return null;
+		}
+		LinkedStack<Integer> path = new LinkedStack<Integer>();
+		for (int x = v; x != s; x = edgeTo[x]) {
+			path.push(x);
+
+		}
+		path.push(s);
+		return path;
+	}
+}
