e15ba2ca83ca5f3f5822a80f3333723372431ff0
Lukun <yimng.lk@gmail.com>
2017-06-21 22:26:47 +0800

    update

--- algorithm/alg/src/alg/graph/Graph.java
+++ algorithm/alg/src/alg/graph/Graph.java
@@ -1,9 +1,13 @@
 package alg.graph;
 
 import alg.basic.Bag;
+import alg.basic.LinkedStack;
 import stdio.In;
+import stdio.StdOut;
 
 public class Graph {
+	private static final String NEWLINE = System.getProperty("line.separator");
+
 	private final int V;
 	private int E;
 	private Bag<Integer>[] adj;
@@ -26,6 +30,20 @@ public class Graph {
 			addEdge(v, w);
 		}
 	}
+	
+	public Graph(Graph G) {
+		this(G.V());
+		this.E = G.E();
+		for (int v = 0; v < G.V(); v++) {
+			LinkedStack<Integer> reverse = new LinkedStack<Integer>();
+			for (int w : G.adj[v]) {
+				reverse.push(w);
+			}
+			for (int w : reverse) {
+				adj[v].add(w);
+			}
+		}
+	}
 
 	public int V() {
 		return V;
@@ -43,5 +61,28 @@ public class Graph {
 
 	public Iterable<Integer> adj(int v) {
 		return adj[v];
+	}
+	
+	public int degree(int v) {
+		return adj[v].size();
+	}
+	
+	public String toString() {
+		StringBuilder s = new StringBuilder();
+		s.append(V + " vertices, " + E + " edges " + NEWLINE);
+		for (int v = 0; v < V; v++) {
+			s.append(v + ": ");
+			for (int w : adj[v]) {
+				s.append(w + " ");
+			}
+			s.append(NEWLINE);
+		}
+		return s.toString();
+	}
+	
+	public static void main(String[]args) {
+		In in = new In(args[0]);
+		Graph G = new Graph(in);
+		StdOut.println(G);
 	}
 }
