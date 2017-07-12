ca6f3ada3e5db13195444eeda25a7f726ccd32b6
Lukun <yimng.lk@gmail.com>
2017-07-12 22:18:08 +0800

    update

---
+++ algorithm/alg/src/alg/graph/DijkstraSP.java
@@ -0,0 +1,160 @@
+package alg.graph;
+
+import java.util.Stack;
+
+import alg.sort.IndexMinPQ;
+import stdio.In;
+import stdio.StdOut;
+
+public class DijkstraSP {
+
+	private DirectedEdge[] edgeTo;
+	private double[] distTo;
+	private IndexMinPQ<Double> pq;
+
+	public DijkstraSP(EdgeWeightedDigraph G, int s) {
+		for (DirectedEdge e : G.edges()) {
+			if (e.weight() < 0)
+				throw new IllegalArgumentException("edge " + e + " has negative weight");
+		}
+		edgeTo = new DirectedEdge[G.V()];
+		distTo = new double[G.V()];
+
+		validateVertex(s);
+
+		pq = new IndexMinPQ<Double>(G.V());
+		for (int v = 0; v < G.V(); v++) {
+			distTo[v] = Double.POSITIVE_INFINITY;
+		}
+		distTo[s] = 0.0;
+		pq.insert(s, 0.0);
+		while (!pq.isEmpty()) {
+			int v = pq.delMin();
+			for (DirectedEdge e : G.adj(v))
+				relax(e);
+		}
+		// check optimality conditions
+		assert check(G, s);
+	}
+
+	// relax edge e and update pq if changed
+	private void relax(DirectedEdge e) {
+		int v = e.from(), w = e.to();
+		if (distTo[w] > distTo[v] + e.weight()) {
+			distTo[w] = distTo[v] + e.weight();
+			edgeTo[w] = e;
+			if (pq.contains(w))
+				pq.decreaseKey(w, distTo[w]);
+			else
+				pq.insert(w, distTo[w]);
+		}
+	}
+
+	public double distTo(int v) {
+		validateVertex(v);
+		return distTo[v];
+	}
+
+	public boolean hasPathTo(int v) {
+		validateVertex(v);
+		return distTo[v] < Double.POSITIVE_INFINITY;
+	}
+
+	public Iterable<DirectedEdge> pathTo(int v) {
+		validateVertex(v);
+		if (!hasPathTo(v))
+			return null;
+		Stack<DirectedEdge> path = new Stack<DirectedEdge>();
+		for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+			path.push(e);
+		}
+		return path;
+	}
+
+	// check optimality conditions:
+	// (i) for all edges e: distTo[e.to()] <= distTo[e.from()] + e.weight()
+	// (ii) for all edge e on the SPT: distTo[e.to()] == distTo[e.from()] +
+	// e.weight()
+	private boolean check(EdgeWeightedDigraph G, int s) {
+
+		// check that edge weights are nonnegative
+		for (DirectedEdge e : G.edges()) {
+			if (e.weight() < 0) {
+				System.err.println("negative edge weight detected");
+				return false;
+			}
+		}
+
+		// check that distTo[v] and edgeTo[v] are consistent
+		if (distTo[s] != 0.0 || edgeTo[s] != null) {
+			System.err.println("distTo[s] and edgeTo[s] inconsistent");
+			return false;
+		}
+		for (int v = 0; v < G.V(); v++) {
+			if (v == s)
+				continue;
+			if (edgeTo[v] == null && distTo[v] != Double.POSITIVE_INFINITY) {
+				System.err.println("distTo[] and edgeTo[] inconsistent");
+				return false;
+			}
+		}
+
+		// check that all edges e = v->w satisfy distTo[w] <= distTo[v] +
+		// e.weight()
+		for (int v = 0; v < G.V(); v++) {
+			for (DirectedEdge e : G.adj(v)) {
+				int w = e.to();
+				if (distTo[v] + e.weight() < distTo[w]) {
+					System.err.println("edge " + e + " not relaxed");
+					return false;
+				}
+			}
+		}
+
+		// check that all edges e = v->w on SPT satisfy distTo[w] == distTo[v] +
+		// e.weight()
+		for (int w = 0; w < G.V(); w++) {
+			if (edgeTo[w] == null)
+				continue;
+			DirectedEdge e = edgeTo[w];
+			int v = e.from();
+			if (w != e.to())
+				return false;
+			if (distTo[v] + e.weight() != distTo[w]) {
+				System.err.println("edge " + e + " on shortest path not tight");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// throw an IllegalArgumentException unless {@code 0 <= v < V}
+	private void validateVertex(int v) {
+		int V = distTo.length;
+		if (v < 0 || v >= V)
+			throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V - 1));
+	}
+
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+		int s = Integer.parseInt(args[1]);
+
+		// compute shortest paths
+		DijkstraSP sp = new DijkstraSP(G, s);
+
+		// print shortest path
+		for (int t = 0; t < G.V(); t++) {
+			if (sp.hasPathTo(t)) {
+				StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+				for (DirectedEdge e : sp.pathTo(t)) {
+					StdOut.print(e + "   ");
+				}
+				StdOut.println();
+			} else {
+				StdOut.printf("%d to %d         no path\n", s, t);
+			}
+		}
+	}
+
+}
