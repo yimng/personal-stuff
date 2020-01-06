fb9bbda5537da62c2b3f9b9157da2eff631e0944
maoqiansheng <980054594@qq.com>
2020-01-06 14:05:44 +0800

    update

---
+++ abs/3/uppercase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# uppercase.sh : Changes input to uppercase.
+
+tr 'a-z' 'A-Z'
+#  Letter ranges must be quoted
+#+ to prevent filename generation from single-letter filenames.
+
+exit 0
